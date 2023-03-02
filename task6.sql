6f914a26c15bc39a5469d4646eb503d569297e89
Daria <procionestrisce@yandex.ru>
2023-03-02 22:38:02 +0300

    дз 6

--- task6.sql
+++ task6.sql
@@ -15,22 +15,30 @@ BEGIN
     IF (sec > 60) THEN
 		SET temp = sec/60;
 		SET sec = sec%60;
+	ELSE
+		RETURN CONCAT("Second ", sec);
 	END IF;
     IF (temp > 60) THEN
 		SET min = temp%60;
 		SET temp = temp/60; 
+	ELSE
+		SET min = temp;
+        RETURN CONCAT("Minutes ", min, ", second ", sec);
 	END IF;
     IF (temp > 24) THEN
 		SET hour = temp%24;
 		SET day = temp/24; 
+    ELSE
+		SET hour = temp;
+        SET result = CONCAT("Day ", day, ", hours  ", hour, ", minutes ", min, ", second ", sec);
 	END IF;
     -- SET result = CONCAT("Day ", day);
-    SET result = CONCAT("Day ", day, ", hours  ", hour, ", minutes ", min, ", second ", sec);
+    
     RETURN result;
     END $$ 
 delimiter ;
 
-SELECT convert_func(123456);
+SELECT convert_func(180);
 
 delimiter $$
 CREATE FUNCTION evan()
